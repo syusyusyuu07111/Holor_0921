91607386ea1c0609df46ab20eae27a0c7cfeebb7
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-21 17:54:27 +0900

    カメラの回転設定

--- Assets/Scripts/TPSCamera.cs
+++ Assets/Scripts/TPSCamera.cs
@@ -4,6 +4,13 @@ using UnityEngine.InputSystem;
 public class TPSCamera : MonoBehaviour
 {
     InputSystem_Actions input;
+    //カメラ入力設定==============================================================================--
+    public float yaw=90;//向いている角度
+    public float RotateSpeed=3.0f;
+    public Transform Camera;
+    public Transform Pivot;
+    public float Distance=3.0f;
+    public Transform cam;
     public void Awake()
     {
         input = new InputSystem_Actions();
@@ -14,12 +21,18 @@ public class TPSCamera : MonoBehaviour
     }
     void Start()
     {
-
+        //カメラの回転の初期パラメータを取得しておく==========================================================
+        if (cam != null) cam=transform;
+        yaw = cam.eulerAngles.y;
     }
-
-    // Update is called once per frame
     void Update()
     {
-
+        //カメラ設定==========================================================================---
+        Vector2 LookInput = input.Player.Look.ReadValue<Vector2>();
+        yaw += LookInput.x * RotateSpeed * Time.deltaTime;
+        Quaternion rot = Quaternion.Euler(0, yaw, 0);
+        Vector3 CameraPos = Pivot.transform.position + rot * new Vector3(0, 0, -Distance);
+        cam.position = CameraPos;
+        cam.LookAt(Pivot.transform.position,Vector3.up);
     }
 }
