9c4b3ddbb6a0723ec48736f9126eda77703a105e
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-21 20:10:47 +0900

    プレイヤー移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    InputSystem_Actions input;
+    InputSystem_Actions Input;
+    [SerializeField] Transform Camera;
+    [SerializeField] float MoveSpeed = 5.0f;
+
 
     private void Awake()
     {
-        input = new InputSystem_Actions();
+        Input = new InputSystem_Actions();
     }
     private void OnEnable()
     {
-        input.Player.Enable();
+        Input.Player.Enable();
     }
     void Start()
     {
@@ -20,10 +23,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //移動==================================================================================-
-        if(input.Player.Move.triggered)
-        {
+        //カメラ座標から移動する方向を取得する=============================================================
+        Vector2 MoveInput = Input.Player.Move.ReadValue<Vector2>();
+        //カメラ座標から前の向きを取得---------------------------------------------------------------------
+        Vector3 Forward = Camera.transform.forward;
+        Forward.y = 0;
+        Forward.Normalize();
+        //カメラ座標から横の向きを取得する-----------------------------------------------------------------
+        Vector3 Right = Camera.transform.right;
+        Right.y = 0;
+        Right.Normalize();
 
+        //プレイヤーの移動==================================================================================
+        {
+            //移動する方向を決めて移動する--------------------------------------------------------------------------
+            Vector3 Movedir = Forward * MoveInput.y + Right * MoveInput.x;
+            if (Movedir.sqrMagnitude > 0.0001f)
+                transform.position += Movedir * Time.deltaTime * MoveSpeed;
         }
     }
 }
