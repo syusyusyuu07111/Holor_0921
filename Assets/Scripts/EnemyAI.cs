9463ec642cde662cf06f900522ba98c19890a18a
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-24 02:39:57 +0900

    生成位置

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,14 +5,22 @@ public class EnemyAI : MonoBehaviour
 {
     bool GhostSpawn=false;
     public Transform Player;
+    public GameObject Ghost;
+    public Vector3 GhostPosition;
     void Start()
     {
+        GhostPosition = new Vector3(Player.transform.position.x+Random.Range(10f,50f),
+        Player.transform.position.y, Player.transform.position.z+Random.Range(10f,50f));
     }
     void Update()
     {
-        if(GhostSpawn)
+        if(GhostSpawn==false)
         {
             StartCoroutine("Spawn");
+        }
+        else if(GhostSpawn==true)
+        {
+            Instantiate(Ghost, GhostPosition, Quaternion.identity);
         }
 
     }
