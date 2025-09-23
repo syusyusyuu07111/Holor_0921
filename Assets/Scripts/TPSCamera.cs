bbd0821071db0079df71ac950ecc0cdc886bef34
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-23 23:18:56 +0900

    ドア開ける

--- Assets/Scripts/TPSCamera.cs
+++ Assets/Scripts/TPSCamera.cs
@@ -15,6 +15,10 @@ public class TPSCamera : MonoBehaviour
     [Header("キャラクター（プレイヤー）")]
     public Transform Player;
     public float AimSpeed=5.0f;
+    public float YawPlayer;
+    public float prevplayerrow;//前フレームのプレイヤーの向いている角度
+    public float Deadyaw = 0.5f;//無視する角度　少しの角度は回転に考慮しない
+    [Range(0f,1f)]float RowAmount=1.0f;//キャラクターとカメラをどのくらい追従させるか
     public void Awake()
     {
         input = new InputSystem_Actions();
@@ -28,6 +32,7 @@ public class TPSCamera : MonoBehaviour
         //カメラの回転の初期パラメータを取得しておく==========================================================
         if (cam != null) cam=transform;
         yaw = cam.eulerAngles.y;
+        prevplayerrow = Player.eulerAngles.y;
     }
     void Update()
     {
@@ -39,7 +44,7 @@ public class TPSCamera : MonoBehaviour
         cam.position = CameraPos;
         cam.LookAt(Pivot.transform.position,Vector3.up);
 
-        //カメラを回転させたらキャラも回転させる
+        //カメラを回転させたらキャラも回転させる--------------------------------------------------
         if(Player!=null)
         {
             quaternion target = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);
