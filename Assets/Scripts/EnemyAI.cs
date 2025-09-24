b0090007b88812e467ee44c7d231d4e7d8bcd23d
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-24 14:28:11 +0900

    幽霊出現

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,32 +1,47 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    bool GhostSpawn=false;
+    bool GhostSpawn = false;
     public Transform Player;
     public GameObject Ghost;
     public Vector3 GhostPosition;
+    public int GhostEncountChance;
+
+    //オーディオ系=========================================================================================
+    public AudioClip SpawnSE;
+    AudioSource audioSource;
     void Start()
     {
-        GhostPosition = new Vector3(Player.transform.position.x+Random.Range(10f,50f),
-        Player.transform.position.y, Player.transform.position.z+Random.Range(10f,50f));
+        StartCoroutine("Spawn");
+        audioSource = GetComponent<AudioSource>();
     }
-    void Update()
+    private void Update()
     {
-        if(GhostSpawn==false)
-        {
-            StartCoroutine("Spawn");
-        }
-        else if(GhostSpawn==true)
-        {
-            Instantiate(Ghost, GhostPosition, Quaternion.identity);
-        }
-
+        GhostPosition = new Vector3(Player.transform.position.x + Random.Range(10f, 50f),
+        Player.transform.position.y, Player.transform.position.z + Random.Range(10f, 50f));
     }
     IEnumerator Spawn()
     {
-
-        yield return new WaitForSeconds(30.0f);
+        //抽選が当たるまでは抽選し続ける==========================================================
+        while (GhostSpawn == false)
+        {
+            GhostEncountChance = Random.Range(0, 50);
+            if (GhostEncountChance > 30)
+            {
+                GhostSpawn = true;
+            }
+            yield return new WaitForSeconds(5.0f);
+        }
+        //抽選が当たった時の処理=================================================================
+        if (GhostSpawn == true)
+        {
+            audioSource.PlayOneShot(SpawnSE);
+            Instantiate(Ghost, GhostPosition, Quaternion.identity);
+            GhostSpawn = false;
+            StartCoroutine("Spawn");
+        }
     }
 }
