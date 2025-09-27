5eb80e883608806d7d79b04cb29905a459c6d292
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-27 20:18:27 +0900

    ボイスランダム再生

--- Assets/Scripts/RandomAudio.cs
+++ Assets/Scripts/RandomAudio.cs
@@ -1,16 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomAudio : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   [SerializeField]private List<AudioClip> ClipList = new();
+    public AudioSource audioSource;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine("PlayBack");
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator PlayBack()
     {
-        
+        while(true)
+        {
+            int idx = Random.Range(0, ClipList.Count);
+            AudioClip clip = ClipList[idx];
+            audioSource.clip = clip;
+            audioSource.Play();
+            yield return new WaitForSeconds(5.0f);
+        }
     }
 }
