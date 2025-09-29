7210879f636ac30dfc25b928a7b9321f7be76877
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-29 19:05:55 +0900

    peek時の判定を取得できるように

--- Assets/Scripts/PeekCamera.cs
+++ Assets/Scripts/PeekCamera.cs
@@ -2,34 +2,53 @@ using UnityEngine;
 
 public class PeekCamera : MonoBehaviour
 {
+    public static PeekCamera Instance;
     public Transform Camera;
     public Transform Player;
     public Transform[] MovePositions;
     private Transform Selectposition;
     InputSystem_Actions input;
-
+    public TPSCamera tps;//カメラ制御
+    public bool IsPeeking { get; set; }
+    private Vector3 savepos;
     private void Awake()
     {
+        Instance = this;
         input = new InputSystem_Actions();
     }
     private void OnEnable()
     {
         input.Player.Enable();
     }
+    private void Start()
+    {
+        IsPeeking = false;
 
+    }
     void Update()
-        //覗く機能　プレイヤーと近いpivotにカメラをよらせる===============================================================
+    //覗く機能　プレイヤーと近いpivotにカメラをよらせる===============================================================
 
     {
         //プレイヤーと近いpivotを探す---------------------------------------------------------------------------------------
         Transform nearest = Nearest(Player.transform.position, MovePositions);
 
-        Debug.Log($"DBG cam:{Camera} player:{Player} pivots:{MovePositions?.Length} nearest:{nearest} openText:{OpenText.instance}");
-
         //プレイヤーに一番近いpivotにカメラを移動させる---------------------------------------------------------------------
-        if (OpenText.instance.CanOpen&&input.Player.Interact.triggered)
+        if (IsPeeking == false && OpenText.instance.CanOpen && input.Player.Interact.triggered)
         {
-            Camera.transform.position = nearest.transform.position;
+            savepos = Camera.transform.position;           // 元位置を保存
+            tps.ControlEnable = false;                      // TPS停止
+            if (nearest != null)
+            {
+                Camera.transform.position = nearest.transform.position;
+            }
+            IsPeeking = true;                               // 覗き状態にする
+        }
+        //カメラ位置戻す------------------------------------------------------------------------------------------------------
+        else if (IsPeeking == true && input.Player.Interact.triggered)
+        {
+            IsPeeking = false;
+            Camera.position = savepos;                      // 戻す
+            tps.ControlEnable = true;                       // TPS再開
         }
     }
     //最寄りのpivotを返す
@@ -41,9 +60,10 @@ public class PeekCamera : MonoBehaviour
         //配列の中の座標を取得する----------------------------------------------------------------------------------------
         foreach (Transform transformpivot in pivots)
         {
+            if (!transformpivot) continue;                 //
             Vector3 pos = transformpivot.position;
             float distance = (transformpivot.position - fromPos).sqrMagnitude;
-            if(distance<best)
+            if (distance < best)
             {
                 best = distance;
                 bestpos = transformpivot;
