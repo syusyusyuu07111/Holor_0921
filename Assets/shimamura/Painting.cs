7b4c39ef8f0cb72c6ebb3363fb0b6d88d251304f
shimamura <shimakou779@gmail.com>
2025-10-30 18:52:12 +0900

    [add]蝋燭を消すと絵が落ちる

---
+++ Assets/shimamura/Painting.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Painting : MonoBehaviour
+{
+
+    public static readonly  List<Painting> PaintingAll = new();
+
+    private Rigidbody _rb;
+
+    private void OnEnable() => PaintingAll.Add(this);
+    private void OnDisable() => PaintingAll.Remove(this);
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// ŠG‰æ‚ð—Ž‚Æ‚·
+    /// </summary>
+    public void Drop()
+    {
+        if(_rb == null)
+        {
+            Debug.Log($"{name}‚ÉRigidbody‚ª‚È‚¢");
+            return;
+        }
+        _rb.isKinematic = false;
+    }
+}
